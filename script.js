5f98d6a30e2ad6dac4dc42883c71a9050708a07f
Olive <141564336+FlamingOlives@users.noreply.github.com>
2024-02-15 09:13:05 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+var plant = 0;
+var mod = 1;
+var clicks = 0;
+
+
+function play() {
+  plant += mod;
+  clicks += mod;
+  let displayCount = count;
+  for (let i = suffixes.length - 1; i >= 1; i--) {
+    const limit = Math.pow(10, i * 3);
+    if (count >= limit) {
+      displayCount = (count / limit).toFixed(1) + suffixes[i];
+      break;
+    }
+  }
+  printerName.innerText = "You have $" + displayCount;
+  tit.innerText = displayCount + " Cats - Catstack";
+  checkAchievements();
+  format();
+}
