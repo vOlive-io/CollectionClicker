0a46b38dad20ba368bb8fad567f401d3328f1e83
Olive <141564336+FlamingOlives@users.noreply.github.com>
2024-02-22 08:46:41 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-var redBer = 0;
-var oraBer = 0;
-var yelBer = 0;
-var greBer = 0;
-var bluBer = 0;
-var purBer = 0;
-var goldBer = 0;
+var coins = 50;
+var berryFer1 = 0;
+var berryFer2 = 0;
+var berryFer3 = 0;
+var berryFer4 = 0;
+var berryFer5 = 0;
+var berryFer6 = 0;
+var berryFer7 = 0;
+var comicBerry = 0;
 var mod = 1;
 var clicks = 0;
 
 
 function play() {
-  redRaz += mod;
+  coins += mod;
   clicks += 1;
 }
 
