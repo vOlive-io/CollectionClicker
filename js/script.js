a8082c55a7a72f5417c6f7bd507c91df8ac213b5
Hanna Skairipa <hanna_boone@icloud.com>
2024-02-24 11:03:24 -0700

    Updated formats

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 /////////////////////////////////////
 //             Berries             //
 /////////////////////////////////////
+
 var coins = 50;
 var berryFer1 = 0;
 var berryFer2 = 0;
@@ -11,11 +12,13 @@ var berryFer6 = 0;
 var berryFer7 = 0;
 var comicBerry = 0;
 var clicks = 0;
-var mod = 1;
+var modifier = 1;
 
 /////////////////////////////////////
 //              Items              //
 /////////////////////////////////////
+
+
 var item1 = 0;
 var item2 = 0;
 var item3 = 0;
@@ -35,24 +38,15 @@ var item16 = 0;
 var item17 = 0;
 
 
+/////////////////////////////////////
+//              Items              //
+/////////////////////////////////////
 
-  
-
-function play() {
-  coins += mod;
-  clicks += 1;
-  //add a part here that 25% of the time function list is called
+const play = () => {
+	coins += modifier;
+	clicks += 1;
 }
-
 
 function list() {
-//add a part here that gives you a random item from the "items" var list. item 1 should be the most common, item 2 a little less, item 3 less than 2. so on and so on.
+	// add a part here that gives you a random item from the "items" var list. item 1 should be the most common, item 2 a little less, item 3 less than 2. so on and so on.
 }
-
-
-
-
-
-
-
-
