b6f7896430d93e9ee304546492f5fdbd1fc23c3f
Hanna Skairipa <hanna_boone@icloud.com>
2024-02-24 11:18:48 -0700

    added tailwind

--- js/script.js
+++ js/script.js
@@ -1,52 +1,53 @@
 /////////////////////////////////////
-//             Berries             //
+//             Resources           //
 /////////////////////////////////////
 
-var coins = 50;
-var berryFer1 = 0;
-var berryFer2 = 0;
-var berryFer3 = 0;
-var berryFer4 = 0;
-var berryFer5 = 0;
-var berryFer6 = 0;
-var berryFer7 = 0;
-var comicBerry = 0;
-var clicks = 0;
-var modifier = 1;
+const resources = {
+	coins: 50,
+	clicks: 0,
+	modifier: 1,
+	berries: {
+		berryFer1: 0,
+		berryFer2: 0,
+		berryFer3: 0,
+		berryFer4: 0,
+		berryFer5: 0,
+		berryFer6: 0,
+		berryFer7: 0,
+		comicBerry: 0,
+	},
+	items: {
+		item1: 0,
+		item2: 0,
+		item3: 0,
+		item4: 0,
+		item5: 0,
+		item6: 0,
+		item7: 0,
+		item8: 0,
+		item9: 0,
+		item10: 0,
+		item11: 0,
+		item12: 0,
+		item13: 0,
+		item14: 0,
+		item15: 0,
+		item16: 0,
+		item17: 0,
+	},
+};
 
 /////////////////////////////////////
-//              Items              //
-/////////////////////////////////////
-
-
-var item1 = 0;
-var item2 = 0;
-var item3 = 0;
-var item4 = 0;
-var item5 = 0;
-var item6 = 0;
-var item7 = 0;
-var item8 = 0;
-var item9 = 0;
-var item10 = 0;
-var item11 = 0;
-var item12 = 0;
-var item13 = 0;
-var item14 = 0;
-var item15 = 0;
-var item16 = 0;
-var item17 = 0;
-
-
-/////////////////////////////////////
-//              Items              //
+//             Functions           //
 /////////////////////////////////////
 
 const play = () => {
-	coins += modifier;
-	clicks += 1;
+	resources.coins += resources.modifier;
+	resources.clicks += 1;
 }
 
-function list() {
-	// add a part here that gives you a random item from the "items" var list. item 1 should be the most common, item 2 a little less, item 3 less than 2. so on and so on.
+const Item = () => {
+	const itemKeys = Object.keys(resources.items);
+	const randomItemKey = itemKeys[Math.floor(Math.random() * itemKeys.length)];
+	resources.items[randomItemKey] += 1;
 }
